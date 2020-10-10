234588edd33a2c24ccc45468e80c861237241c8b
lhead <lssdima@yandex.ru>
2020-10-10 22:56:09 +0300

    deleted:    prev

--- README.md
+++
@@ -1,2 +0,0 @@
-# exchange_tool
-analising and visualising data from bestchange
--- data_processing/tests/test_db_processing.py
+++
@@ -1,5 +0,0 @@
-from data_processing.db_processing import update_currency
-
-def test_update_currency():
-    new_currency = update_currency()
-    assert new_currency == True
