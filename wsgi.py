6b83020503d4be476913223e8a6843a7f2fc1bd4
lhead <lssdima@yandex.ru>
2020-10-11 01:57:21 +0300

    2st_working_examplee

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
+from app import app
+application = app.server
 """Application entry point."""
 
-from plotly_flask import init_app
+# from plotly_flask import init_app
 
-app = init_app()
+# app = init_app()
 
-if __name__ == "__main__":
-	app.run(host='0.0.0.0', debug=True)
+# if __name__ == "__main__":
+# 	app.run(host='0.0.0.0', debug=True)
