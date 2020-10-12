02cc9b52252c8c4c31fd5c03aac52ee64328ca03
Andrey Pindak <AIPindak@sberbank.ru>
2020-10-12 19:52:42 -0400

    формы кнопок и поиск внутри кнопок сделан

--- app.py
+++ app.py
@@ -1,48 +1,38 @@
-import os
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 from apscheduler.schedulers.background import BackgroundScheduler
-import time
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-import plotly.graph_objs as go
-import pandas as pd
-from pathlib import Path
 from data_processing.data_processing import (
-    # update_data,
     pair_find_leader,
     find_cp_leaderpoints_minutes,
     find_cp_last_datetime,
 )
+from db.db_model import Currency
 from sm import app, db
 from data_processing.db_processing import update_data
-from db.db_model import Currency, Saler, Currency_pair
-from globals import (
-    minute2_datetime_label_g,
-    minute10_datetime_label_g,
-    hour_datetime_label_g,
-    day_datetime_label_g,
-)
+from dash.exceptions import PreventUpdate
 
+
+session = db.session
 
 colors = {"background": "#111111", "text": "#7FDBFF"}
 
-c_give_num = 29
-c_take_num = 139
+# currency_data = pd.read_csv('../datadir/bm_cy.csv', )
 
-session = db.session
+data = pd.read_sql(
+    db.session.query(Currency.num, Currency.name)
+        .statement,
+    db.session.bind
+)
+options = [{"label": x[1], "value": x[0]} for x in data.values]
+
 
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(update_data, args=[db.session, sched], trigger="interval", minutes=2)
 sched.start()
-
-c_give_num = 29
-c_take_num = 139
-fig = None
-
 
 markdown_text = """
 Данные о курсах взяты c сайта https://www.bestchange.net
@@ -59,33 +49,40 @@ app.layout = html.Div(
         dcc.Markdown(
             markdown_text, style={"textAlign": "center", "color": colors["text"]}
         ),
-        dcc.Dropdown(id="give_currency_dropdown",
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': 'Montreal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        placeholder="Выберите отдаваемую валюту",
-        ),
-        dcc.Dropdown(id="take_currency_dropdown",
-            options=[
-                {'label': 'New York City', 'value': 'NYC'},
-                {'label': 'Montreal', 'value': 'MTL'},
-                {'label': 'San Francisco', 'value': 'SF'}
-            ],
-            placeholder="Выберите получаемую валюту",
-        ),
+        html.Label(["Выберите отдаваемую валюту", dcc.Dropdown(id="give_currency_dropdown_dynamic",
+        placeholder="список валют")]),
+        html.Label(["Выберите получаемую валюту", dcc.Dropdown(id="take_currency_dropdown_dynamic",
+        placeholder = "список валют")]),
         dcc.Graph(id="live_update_graph"),
         dcc.Interval(
             id='interval-component',
             interval=1 * 1000 * 50, # в милисекундах
             n_intervals=0
         )
-        # dcc.Slider(
-        #
-        # )
     ],
 )
+@app.callback(
+    dash.dependencies.Output("give_currency_dropdown_dynamic", "options"),
+    [dash.dependencies.Input("give_currency_dropdown_dynamic", "search_value")],
+)
+def update_options(search_value):
+    # if not search_value:
+    #     raise PreventUpdate
+    if not search_value:
+        return options
+    return [o for o in options if search_value in o["label"]]
+
+
+@app.callback(
+    dash.dependencies.Output("take_currency_dropdown_dynamic", "options"),
+    [dash.dependencies.Input("take_currency_dropdown_dynamic", "search_value")],
+)
+def update_options(search_value):
+    # if not search_value:
+    #     raise PreventUpdate
+    if not search_value:
+        return options
+    return [o for o in options if search_value in o["label"]]
 
 @app.callback(
     Output(component_id='live_update_graph', component_property='figure'),
