88034669471770f15ae0a377fe789be645267d76
lhead <lssdima@yandex.ru>
2020-10-22 22:37:07 +0300

    Changes to be committed: 	new file:   start.sh 	new file:   wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+from app import app
+application = app.server
+"""Application entry point."""
+
+# from plotly_flask import init_app
+
+# app = init_app()
+
+# if __name__ == "__main__":
+# 	app.run(host='0.0.0.0', debug=True)
